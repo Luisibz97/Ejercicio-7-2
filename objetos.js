e533269178d441c10d0b5b8d2a630a276231fbd4
Luisibz97 <luis_rab_20@hotmail.com>
2022-07-20 23:36:52 -0300

    first commit

---
+++ objetos.js
@@ -0,0 +1,32 @@
+
+const data = {
+    nombre: 'Luis', 
+    apellido: 'Ibañez' ,
+    edad: 24,
+    altura: 1.87,
+    eresDesarrollador: true
+}
+
+const myAge = data.edad
+
+
+const list = [ data, 
+{   
+    nombre: 'Juan', 
+    apellido: 'Navarro' ,
+    edad: 22,
+    altura: 1.69,
+    eresDesarrollador: false
+},
+{
+    nombre: 'Cristian', 
+    apellido: 'Villafañe' ,
+    edad: 25,
+    altura: 1.85,
+    eresDesarrollador: false
+}]
+
+
+const newlist = [...list].sort(( a, b ) => a.edad - b.edad)
+
+
